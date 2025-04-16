13d2531244955a18f7ef9c46a14d62830380f52a
Granit16 <damirm@rambler.ru>
2025-04-16 18:59:50 +0500

    Create bucket.tf

---
+++ CloudProjectOrganisation/03/bucket.tf
@@ -0,0 +1,25 @@
+resource "yandex_storage_bucket" "test-bucket" {
+  bucket = var.bucket_name
+  acl    = "public-read"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = yandex_kms_symmetric_key.netology-key.id
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+}
+
+resource "yandex_storage_object" "upload_pic" {
+  bucket     = var.bucket_name
+  key        = "man.jpeg"
+  source     = "media/gomer.jpeg"
+  acl    = "public-read"
+  content_type = "image/jpeg"
+
+  depends_on = [
+    yandex_storage_bucket.test-bucket
+  ]
+}
